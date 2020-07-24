495ffd55259071a47d31c8eb15b70f542af7ebf7
Stanisław Wasik <st.wasik@gmail.com>
2020-07-24 23:21:41 +0200

    added removing grid from paper (funtions in separate file)

---
+++ remove_lines.py
@@ -0,0 +1,29 @@
+def erode(img, kernel_size=2):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size))
+    return cv.erode(img, kernel)
+
+def dilate(img, kernel_size=2):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size))
+    return cv.dilate(img, kernel)
+
+def remove_lines(img):
+    gray = img.copy()
+    
+    horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (96,1))
+    h_lines = 255 - cv.filter2D(gray, 0, horizontal_kernel)
+    
+    vertical_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1,96))
+    v_lines = 255 - cv.filter2D(gray, 0, vertical_kernel)
+
+    mask1 = v_lines + h_lines
+    mask1 = dilate(mask1, 2)
+    
+    gray = erode(dilate(np.invert(gray), 4), 4)
+    gray = gray - mask1
+    gray = np.invert(gray)
+    gray = dilate(gray, 8)
+    gray = erode(gray, 20)
+    gray = dilate(gray, 10)
+    return gray
